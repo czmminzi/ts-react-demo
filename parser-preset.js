209c2b2801ec425a20c27246dca093ce9b59c02f
chenzimin002 <chenzimin002@ke.com>
2018-11-13 20:30:24 +0800

    [chore]修改

--- parser-preset.js
+++ parser-preset.js
@@ -1,7 +1,7 @@
 module.exports = {
   parserOpts : {
     // headerPattern: /^\[(\w*)\](\w*)&/,
-    headerPattern: /^\[(feat|fix|docs|style|refactor|test|chore)\]/,
+    headerPattern: /^\[(feat|fix|docs|style|refactor|test|chore)\](.*)/,
     headerCorrespondence: ['type', 'subject']
   }
 };
